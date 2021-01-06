d20719b3fd07d4ca30a8003c6fb8eabaa9c53146
ammarboudhrioua <ammar.boudhrioua@gmail.com>
2021-01-06 16:47:35 +0100

    oke

--- models/votes.js
+++ models/votes.js
@@ -3,6 +3,7 @@ const mongoose =require('mongoose');
 var schema = mongoose.Schema
 
 var voteSchema =new schema({
+    idvoter:{type:String},
     choice:{type:Boolean},
 })
 module.exports=mongoose.model('votes',voteSchema);
--- routes/login.js
+++ routes/login.js
@@ -3,17 +3,37 @@ var router = express.Router();
 var user = require('../models/users');
 
     
-router.get('/getAuth', (req, res, next) => {
-    user.findOne({email: req.body.email}).then((u) => {
-        if(u &&( u.password === req.body.password)){
+router.post('/getAuth', (req, res, next) => {
+
+  
+    user.findOne({
+            email: req.body.email,
+            password: req.body.password
+        }).then((u) => {
+    
       res.status(200).json({
         message: "authentification Valide",
         User: u
       })
+  
     }
-    }).catch(err => {
+    ).catch(err => {
       console.log(err);
     });
   
   });
+// router.post('/getAuth', (req, res, next) => {
+//   user.findOne({
+//       email: req.body.email,
+//       password: req.body.password
+//   }).then((u) => {
+//     res.status(200).json({
+//       message: "user login",
+//       User: u
+//     })
+//   }).catch(err => {
+//     console.log(err);
+//   });
+
+// });
 module.exports = router;
